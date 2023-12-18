b2cca24d40f958b3a052ec3ff47bdb220ac55f26
yipei <studentypz@gmail.com>
2023-12-18 15:33:53 +0100

    logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+/* import { useDarkModeContext } from "../features/contexts/DarkModeContext"; */
+
+const StyledLogo = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  height: 9.6rem;
+  width: auto;
+`;
+
 function Logo() {
-  return <div>Logo</div>;
+  /* const { isDarkMode } = useDarkModeContext(); */
+  return (
+    <StyledLogo>
+      <Img src="/logo-light.png" alt="Logo" />
+    </StyledLogo>
+  );
 }
 
 export default Logo;
