841c8037d6203fc9ef7e39008a8d8af0e375ce68
yipei <studentypz@gmail.com>
2023-12-22 15:25:35 +0100

    exercise

--- src/apps/ExerciseGenerator/Excercise.jsx
+++ src/apps/ExerciseGenerator/Excercise.jsx
@@ -1,34 +1,62 @@
 import { useState } from "react";
-import { HiChevronDown } from "react-icons/hi2";
+import { HiChevronDown, HiMiniStar } from "react-icons/hi2";
 
 import Dropdownbar from "../../ui/Dropdownbar";
+import styled from "styled-components";
+
+const ExerciseTable = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  width: 100%;
+  background-color: white;
+  & span {
+    display: block;
+    margin-bottom: 5px;
+  }
+`;
 
 function Excercise({ exercise }) {
-  const { name, type, diff, set, rep, rest, favortite } = exercise;
-  const { display, setDisplay } = useState(false);
+  const { name, type, diff, set, rep, rest, favorite } = exercise;
+  const [display, setDisplay] = useState(false);
+
+  function handleDisplay() {
+    console.log("clicked");
+    setDisplay((d) => !d);
+  }
   return (
     <div>
       {diff === "easy" && (
-        <Dropdownbar
-          size="large"
-          variation="success"
-          onClick={() => setDisplay((d) => !d)}
-        >
+        <Dropdownbar size="large" variation="success" onClick={handleDisplay}>
           {name}
           <HiChevronDown />
         </Dropdownbar>
       )}
       {diff === "medium" && (
-        <Dropdownbar size="large" variation="secondary">
+        <Dropdownbar size="large" variation="primary" onClick={handleDisplay}>
           {name}
+          <HiChevronDown />
         </Dropdownbar>
       )}
       {diff === "hard" && (
-        <Dropdownbar size="large" variation="danger">
+        <Dropdownbar size="large" variation="danger" onClick={handleDisplay}>
           {name}
+          <HiChevronDown />
         </Dropdownbar>
       )}
-      {display && <div></div>}
+      {display && (
+        <ExerciseTable>
+          <span>name: {name} </span>
+          <span>type: {type} </span>
+          <span>difficulity: {diff} </span>
+          <span>sets: {set} </span>
+          <span>reps: {rep} </span>
+          <span>resting time: {rest}s </span>
+          <span>favorite: {favorite && <HiMiniStar />} </span>
+        </ExerciseTable>
+      )}
     </div>
   );
 }
